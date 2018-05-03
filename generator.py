83fffdb9a5122e18756fd4a3fad43e95bd8b7fa5
Paul Podolny <ppodolny@users.noreply.github.com>
2018-05-03 10:20:48 -0700

    Update generator.py

--- generator.py
+++ generator.py
@@ -5,5 +5,5 @@ from faker import Faker
 
 fake = Faker('en_CA')
 for n in range(1000000):
-    print(n+","+fake.name()+","+fake.province()+","+fake.company()+","+str(random.randint(18,90)))
-    #print(n+","+fake.job())
+    print(str(n)+","+fake.name()+","+fake.province()+","+fake.company()+","+str(random.randint(18,90)))
+    #print(str(n)+","+fake.job())
